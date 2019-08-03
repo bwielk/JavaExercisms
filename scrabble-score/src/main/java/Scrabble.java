f9f13cd24f4ee183a070a4ca78b3a743364062d9
bwielk <bwielk@gmail.com>
2019-08-04 00:19:58 +0200

    New solution

--- scrabble-score/src/main/java/Scrabble.java
+++ scrabble-score/src/main/java/Scrabble.java
@@ -1,34 +1,57 @@
 import java.util.*;
 
+enum ScrabbleScores{
+
+    VALUE_1(new String[]{"A", "E", "O", "I", "U", "L", "N", "R", "S", "T"}, 1),
+    VALUE_2(new String[]{"D", "G"}, 2),
+    VALUE_3(new String[]{"B", "C", "M", "P"}, 3),
+    VALUE_4(new String[]{"F", "H", "V", "W", "Y"}, 4),
+    VALUE_5(new String[]{"K"}, 5),
+    VALUE_8(new String[]{"J", "X"}, 8),
+    VALUE_10(new String[]{"Q", "Z"}, 10);
+
+    private String[] characters;
+    private Integer value;
+
+    ScrabbleScores(String[] characters, Integer value){
+        this.characters = characters;
+        this.value = value;
+    }
+
+    ArrayList<String> getCharacters(){
+        return new ArrayList<>(Arrays.asList(this.characters));
+    }
+
+    Integer getValue(){
+        return this.value;
+    }
+}
+
 class Scrabble {
 
     private ArrayList<String> listOfChars;
-    private Map<ArrayList<String>, Integer> values;
+    private Map<String, Integer> values;
     private int result;
 
     Scrabble(String word) {
-        this.listOfChars = new ArrayList<>(Arrays.asList(word.replace(" ", "").split("")));
+        this.listOfChars = !word.equals("") ? new ArrayList<>(Arrays.asList(word.replace(" ", "").split(""))) : null;
         this.result = 0;
         this.values = new HashMap<>();
-        this.values.put(new ArrayList<>(Arrays.asList("A", "E", "I", "O", "U", "L", "N", "R", "S", "T")), 1);
-        this.values.put(new ArrayList<>(Arrays.asList("D", "G")), 2);
-        this.values.put(new ArrayList<>(Arrays.asList("B", "C", "M", "P")), 3);
-        this.values.put(new ArrayList<>(Arrays.asList("F", "H", "V", "W", "Y")), 4);
-        this.values.put(new ArrayList<>(Arrays.asList("K")), 5);
-        this.values.put(new ArrayList<>(Arrays.asList("J", "X")), 8);
-        this.values.put(new ArrayList<>(Arrays.asList("Q", "Z")), 10);
+        for(ScrabbleScores scoreChart : ScrabbleScores.values()){
+            for(String character : scoreChart.getCharacters()){
+                this.values.put(character, scoreChart.getValue());
+            }
+        }
     }
 
     int getScore() {
-        Set<ArrayList<String>> characters = this.values.keySet();
-        for(String characterInWord : listOfChars)
-            for (List<String> setOfchars : characters){
-                for(String character : setOfchars){
-                    if(character.equals(characterInWord.toUpperCase())){
-                        this.result += this.values.get(setOfchars);
-                    }
-                }
+        if(this.listOfChars == null){
+            return this.result;
+        }else {
+            for(String character : this.listOfChars) {
+                this.result += this.values.get(character.toUpperCase());
             }
+        }
         return this.result;
     }
 }
