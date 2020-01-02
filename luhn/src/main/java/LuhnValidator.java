71feb1acdde466c89fe94262db43c38ef3c58dc2
bwielk <bwielk@gmail.com>
2020-01-02 23:18:54 +0100

    All test pass. Refactor required.

--- luhn/src/main/java/LuhnValidator.java
+++ luhn/src/main/java/LuhnValidator.java
@@ -5,11 +5,17 @@ import java.util.stream.Collectors;
 class LuhnValidator {
 
     boolean isValid(String candidate) {
+        candidate = candidate.replace(" ", "");
         if (candidate.length() < 2) {
             return false;
         }else{
-            List<Integer> candidateAsList = Arrays.asList(candidate.replace(" ", "").split(""))
-                    .stream().map(x -> Integer.parseInt(x)).collect(Collectors.toList());
+            List<Integer> candidateAsList;
+            try{
+                 candidateAsList = Arrays.asList(candidate.split(""))
+                        .stream().map(x -> Integer.parseInt(x)).collect(Collectors.toList());
+            }catch(NumberFormatException e){
+                return false;
+            }
             for(int i = candidateAsList.size()-2; i>=0; i-=2){
                 int numberToDouble = candidateAsList.get(i)*2;
                 if(numberToDouble>9){
--- luhn/src/test/java/LuhnValidatorTest.java
+++ luhn/src/test/java/LuhnValidatorTest.java
@@ -67,54 +67,54 @@ public class LuhnValidatorTest {
     }
 
 
-//    @Test
-//    public void testValidStringsWithANonDigitAtEndInvalid() {
-//        assertFalse(luhnValidator.isValid("059a"));
-//    }
-//
-//
-//    @Test
-//    public void testStringContainingPunctuationInvalid() {
-//        assertFalse(luhnValidator.isValid("055-444-285"));
-//    }
-//
-//
-//    @Test
-//    public void testStringContainingSymbolsInvalid() {
-//        assertFalse(luhnValidator.isValid("055# 444$ 285"));
-//    }
-//
-//
-//    @Test
-//    public void testSingleSpaceWithZeroInvalid() {
-//        assertFalse(luhnValidator.isValid(" 0"));
-//    }
-//
-//
-//    @Test
-//    public void testMoreThanSingleZeroValid() {
-//        assertTrue(luhnValidator.isValid("0000 0"));
-//    }
-//
-//
-//    @Test
-//    public void testDigitNineConvertedToOutputNine() {
-//        assertTrue(luhnValidator.isValid("091"));
-//    }
-//
-//
-//
-//    @Test
-//    public void testUsingASCIIValueForNonDoubledNonDigitNotAllowed() {
-//        assertFalse(luhnValidator.isValid("055b 444 285"));
-//    }
-//
-//
-//    @Test
-//    public void testUsingASCIIValueForDoubledNonDigitNotAllowed() {
-//        assertFalse(luhnValidator.isValid(":9"));
-//    }
-//
+    @Test
+    public void testValidStringsWithANonDigitAtEndInvalid() {
+        assertFalse(luhnValidator.isValid("059a"));
+    }
+
+
+    @Test
+    public void testStringContainingPunctuationInvalid() {
+        assertFalse(luhnValidator.isValid("055-444-285"));
+    }
+
+
+    @Test
+    public void testStringContainingSymbolsInvalid() {
+        assertFalse(luhnValidator.isValid("055# 444$ 285"));
+    }
+
+
+    @Test
+    public void testSingleSpaceWithZeroInvalid() {
+        assertFalse(luhnValidator.isValid(" 0"));
+    }
+
+
+    @Test
+    public void testMoreThanSingleZeroValid() {
+        assertTrue(luhnValidator.isValid("0000 0"));
+    }
+
+
+    @Test
+    public void testDigitNineConvertedToOutputNine() {
+        assertTrue(luhnValidator.isValid("091"));
+    }
+
+
+
+    @Test
+    public void testUsingASCIIValueForNonDoubledNonDigitNotAllowed() {
+        assertFalse(luhnValidator.isValid("055b 444 285"));
+    }
+
+
+    @Test
+    public void testUsingASCIIValueForDoubledNonDigitNotAllowed() {
+        assertFalse(luhnValidator.isValid(":9"));
+    }
+
 //    /* The following test diverges from the canonical test data. This is because the corresponding canonical test does
 //     * not account for Java specific functions (such as Character.getNumericValue()), which can be part of incorrect yet
 //     * passing implementations. For more detail, check out issue #972 here:
