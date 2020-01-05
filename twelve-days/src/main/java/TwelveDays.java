e274338d18ecef59c50e2dbce29a6eab4cf955c9
bwielk <bwielk@gmail.com>
2020-01-05 10:41:30 +0100

    All twelve verses can be built

--- twelve-days/src/main/java/TwelveDays.java
+++ twelve-days/src/main/java/TwelveDays.java
@@ -2,7 +2,18 @@ class TwelveDays {
 
     String verseCore = "On the %s day of Christmas my true love gave to me: ";
 
-    String[] gifts = {"a Partridge in a Pear Tree", "two Turtle Doves"};
+    String[] gifts = {"a Partridge in a Pear Tree",
+            "two Turtle Doves",
+            "three French Hens",
+            "four Calling Birds",
+            "five Gold Rings",
+            "six Geese-a-Laying",
+            "seven Swans-a-Swimming",
+            "eight Maids-a-Milking",
+            "nine Ladies Dancing",
+            "ten Lords-a-Leaping",
+            "eleven Pipers Piping",
+            "twelve Drummers Drumming"};
     String[] ordinals = {"", "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth",
     "eleventh", "twelfth"};
 
@@ -10,9 +21,13 @@ class TwelveDays {
         String createdVerse = String.format(verseCore, ordinals[verseNumber]);
         for(int index = verseNumber-1; index>-1; index--){
             createdVerse += gifts[index];
-            //here I am trying to predict if there is a next gift to add to the line. If so, we need to add "and"
+            //here I am trying to figure out if a comma needs to be added
             if(index-1>-1){
-                createdVerse += ", and ";
+                createdVerse += ", ";
+            }
+            //here I am trying to predict if there is a last gift to add to the line. If so, we need to add "and"
+            if(index-1 == 0){
+                createdVerse += "and ";
             }
         }
         createdVerse+=".\n";
--- twelve-days/src/test/java/TwelveDaysTest.java
+++ twelve-days/src/test/java/TwelveDaysTest.java
@@ -27,96 +27,96 @@ public class TwelveDaysTest {
         assertEquals(expectedVerseTwo, twelveDays.verse(2));
     }
 
-//
-//    @Test
-//    public void testVerseThree() {
-//        String expectedVerseThree = "On the third day of Christmas my true love gave to me: three French Hens, " +
-//                "two Turtle Doves, and a Partridge in a Pear Tree.\n";
-//        assertEquals(expectedVerseThree, twelveDays.verse(3));
-//    }
-//
-//
-//    @Test
-//    public void testVerseFour() {
-//        String expectedVerseFour = "On the fourth day of Christmas my true love gave to me: four Calling Birds, " +
-//                "three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n";
-//        assertEquals(expectedVerseFour, twelveDays.verse(4));
-//    }
-//
-//
-//    @Test
-//    public void testVerseFive() {
-//        String expectedVerseFive = "On the fifth day of Christmas my true love gave to me: five Gold Rings, " +
-//                "four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n";
-//        assertEquals(expectedVerseFive, twelveDays.verse(5));
-//    }
-//
-//
-//    @Test
-//    public void testVerseSix() {
-//        String expectedVerseSix = "On the sixth day of Christmas my true love gave to me: six Geese-a-Laying, " +
-//                "five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, " +
-//                "and a Partridge in a Pear Tree.\n";
-//        assertEquals(expectedVerseSix, twelveDays.verse(6));
-//    }
-//
-//
-//    @Test
-//    public void testVerseSeven() {
-//        String expectedVerseSeven = "On the seventh day of Christmas my true love gave to me: " +
-//                "seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, " +
-//                "two Turtle Doves, and a Partridge in a Pear Tree.\n";
-//        assertEquals(expectedVerseSeven, twelveDays.verse(7));
-//    }
-//
-//
-//    @Test
-//    public void testVerseEight() {
-//        String expectedVerseEight = "On the eighth day of Christmas my true love gave to me: eight Maids-a-Milking," +
-//                " seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, " +
-//                "three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n";
-//        assertEquals(expectedVerseEight, twelveDays.verse(8));
-//    }
-//
-//
-//    @Test
-//    public void testVerseNine() {
-//        String expectedVerseNine = "On the ninth day of Christmas my true love gave to me: nine Ladies Dancing, " +
-//                "eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, " +
-//                "four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n";
-//        assertEquals(expectedVerseNine, twelveDays.verse(9));
-//    }
-//
-//
-//    @Test
-//    public void testVerseTen() {
-//        String expectedVerseTen = "On the tenth day of Christmas my true love gave to me: ten Lords-a-Leaping, " +
-//                "nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, " +
-//                "five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, " +
-//                "and a Partridge in a Pear Tree.\n";
-//        assertEquals(expectedVerseTen, twelveDays.verse(10));
-//    }
-//
-//
-//    @Test
-//    public void testVerseEleven() {
-//        String expectedVerseEleven = "On the eleventh day of Christmas my true love gave to me: " +
-//                "eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, " +
-//                "seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, " +
-//                "three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n";
-//        assertEquals(expectedVerseEleven, twelveDays.verse(11));
-//    }
-//
-//
-//    @Test
-//    public void testVerseTwelve() {
-//        String expectedVerseTwelve = "On the twelfth day of Christmas my true love gave to me: " +
-//                "twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, " +
-//                "eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, " +
-//                "four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n";
-//        assertEquals(expectedVerseTwelve, twelveDays.verse(12));
-//    }
-//
+
+    @Test
+    public void testVerseThree() {
+        String expectedVerseThree = "On the third day of Christmas my true love gave to me: three French Hens, " +
+                "two Turtle Doves, and a Partridge in a Pear Tree.\n";
+        assertEquals(expectedVerseThree, twelveDays.verse(3));
+    }
+
+
+    @Test
+    public void testVerseFour() {
+        String expectedVerseFour = "On the fourth day of Christmas my true love gave to me: four Calling Birds, " +
+                "three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n";
+        assertEquals(expectedVerseFour, twelveDays.verse(4));
+    }
+
+
+    @Test
+    public void testVerseFive() {
+        String expectedVerseFive = "On the fifth day of Christmas my true love gave to me: five Gold Rings, " +
+                "four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n";
+        assertEquals(expectedVerseFive, twelveDays.verse(5));
+    }
+
+
+    @Test
+    public void testVerseSix() {
+        String expectedVerseSix = "On the sixth day of Christmas my true love gave to me: six Geese-a-Laying, " +
+                "five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, " +
+                "and a Partridge in a Pear Tree.\n";
+        assertEquals(expectedVerseSix, twelveDays.verse(6));
+    }
+
+
+    @Test
+    public void testVerseSeven() {
+        String expectedVerseSeven = "On the seventh day of Christmas my true love gave to me: " +
+                "seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, " +
+                "two Turtle Doves, and a Partridge in a Pear Tree.\n";
+        assertEquals(expectedVerseSeven, twelveDays.verse(7));
+    }
+
+
+    @Test
+    public void testVerseEight() {
+        String expectedVerseEight = "On the eighth day of Christmas my true love gave to me: eight Maids-a-Milking," +
+                " seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, " +
+                "three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n";
+        assertEquals(expectedVerseEight, twelveDays.verse(8));
+    }
+
+
+    @Test
+    public void testVerseNine() {
+        String expectedVerseNine = "On the ninth day of Christmas my true love gave to me: nine Ladies Dancing, " +
+                "eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, " +
+                "four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n";
+        assertEquals(expectedVerseNine, twelveDays.verse(9));
+    }
+
+
+    @Test
+    public void testVerseTen() {
+        String expectedVerseTen = "On the tenth day of Christmas my true love gave to me: ten Lords-a-Leaping, " +
+                "nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, " +
+                "five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, " +
+                "and a Partridge in a Pear Tree.\n";
+        assertEquals(expectedVerseTen, twelveDays.verse(10));
+    }
+
+
+    @Test
+    public void testVerseEleven() {
+        String expectedVerseEleven = "On the eleventh day of Christmas my true love gave to me: " +
+                "eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, " +
+                "seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, " +
+                "three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n";
+        assertEquals(expectedVerseEleven, twelveDays.verse(11));
+    }
+
+
+    @Test
+    public void testVerseTwelve() {
+        String expectedVerseTwelve = "On the twelfth day of Christmas my true love gave to me: " +
+                "twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, " +
+                "eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, " +
+                "four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n";
+        assertEquals(expectedVerseTwelve, twelveDays.verse(12));
+    }
+
 //
 //    @Test
 //    public void testFirstThreeVerses() {
