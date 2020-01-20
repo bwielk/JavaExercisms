d4ccf1285c095ba8313fb6be8cf4f6c690909b6c
bwielk <bwielk@gmail.com>
2020-01-20 21:12:26 +0100

    Grains challenge completed

--- grains/src/main/java/Grains.java
+++ grains/src/main/java/Grains.java
@@ -7,9 +7,12 @@ class Grains {
     private List<BigInteger> grainsOnBoard;
 
     BigInteger grainsOnSquare(final int square) {
+        if(square <= 0 || square > 64){
+            throw new IllegalArgumentException("square must be between 1 and 64");
+        }
         grainsOnBoard = new ArrayList<>();
         grainsOnBoard.add(new BigInteger(String.valueOf(1)));
-        for(int i=0; i<square; i++){
+        for(int i=0; i<square-1; i++){
             grainsOnBoard.add(new BigInteger(String.valueOf(grainsOnBoard.get(i))).multiply(new BigInteger(String.valueOf(2))));
         }
         return grainsOnBoard.get(square-1);
@@ -19,7 +22,7 @@ class Grains {
         BigInteger result = new BigInteger(String.valueOf(0));
         grainsOnBoard = new ArrayList<>();
         grainsOnBoard.add(new BigInteger(String.valueOf(1)));
-        grainsOnSquare(63);
+        grainsOnSquare(64);
         return grainsOnBoard.stream().reduce(BigInteger.ZERO, (x, y) -> x.add(y));
     }
 }
