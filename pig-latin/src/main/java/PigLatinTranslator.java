941c21d33297c1bda7c0ce3a637b6462d059fe33
bwielk <bwielk@gmail.com>
2020-04-09 22:12:50 +0200

    length encoding - solution completed

---
+++ pig-latin/src/main/java/PigLatinTranslator.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.List;
+
+class PigLatinTranslator{
+
+    private List<String> vowels;
+
+    public PigLatinTranslator(){
+        vowels = Arrays.asList("aeiou".split(""));
+    }
+
+    public String translate(String word) {
+        String transformedWord = null;
+        if(vowels.contains(String.valueOf(word.charAt(0)))){
+            transformedWord = word + "ay";
+        }else{
+            transformedWord = word.substring(1,word.length()-1) + word.charAt(0) + "ay";
+        }
+        return transformedWord;
+    }
+}
+
