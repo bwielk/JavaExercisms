ce5f59e7b54454d6e5d3d63e5303a89440bc5a7f
bwielk <bwielk@gmail.com>
2020-04-11 19:03:25 +0200

    allergies - basic structure introduced

---
+++ allergies/src/main/java/Allergies.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+class Allergies {
+
+    private Integer allergyScore;
+    private List<Integer> allergyValues;
+    private List<Allergen> allergies;
+
+    public Allergies(Integer allergyScore){
+        this.allergyScore = allergyScore;
+        this.allergyValues = Arrays.stream(Allergen.values()).map(Allergen::getScore).collect(Collectors.toList());
+        Collections.reverse(allergyValues);
+        this.allergies = new ArrayList<>();
+        for(Integer value : allergyValues){
+            if(allergyScore >= value){
+                allergyScore-= value;
+                allergies.add(Arrays.stream(Allergen.values()).filter(x -> x.getScore() == value).collect(Collectors.toList()).get(0));
+            }
+        }
+    }
+
+    public boolean isAllergicTo(Allergen allergen){
+        return allergies.contains(allergen);
+    }
+
+    public List<Allergen> getList(){
+        return this.allergies;
+    }
+}
