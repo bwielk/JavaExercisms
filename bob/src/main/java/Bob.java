e8f294d066fbb35f4290f4986155b7ee9cfd9e43
bwielk <bwielk@gmail.com>
2020-04-11 18:04:39 +0200

    bob - resolved

--- bob/src/main/java/Bob.java
+++ bob/src/main/java/Bob.java
@@ -5,16 +5,22 @@ class Bob{
                 "[\"\\#$%&()*+,\n\r\t" +
                         "\\-./:;<=>@\\[\n" +
                         "\\\\\\]^_‘{|}~]", "")
+                .replaceAll(" ", "")
                 .trim();
-        if(unnecessaryPunctuationFree.replaceAll(" ", "").isEmpty()){
+        if(unnecessaryPunctuationFree.isEmpty()){
             return "Fine. Be that way!";
         }
-        if(unnecessaryPunctuationFree.equals(unnecessaryPunctuationFree.toUpperCase())
-                && unnecessaryPunctuationFree.charAt(unnecessaryPunctuationFree.length()-1) == '?'){
-            return "Calm down, I know what I'm doing!";
+        if(unnecessaryPunctuationFree.matches("[0-9]+")){//digits w/o q mark
+            return "Whatever.";
         }
         if(unnecessaryPunctuationFree.charAt(unnecessaryPunctuationFree.length()-1) == '?'){
-            return "Sure.";
+            if(!unnecessaryPunctuationFree.equals(unnecessaryPunctuationFree.toUpperCase())//uppercase with q mark
+            || unnecessaryPunctuationFree.matches("[0-9]+\\?")//digits with a question mark at the end
+            || (unnecessaryPunctuationFree.charAt(0) == '?' && unnecessaryPunctuationFree.length() == 1)){ //special chars (punctuation) cleared off at the very beginning
+                return "Sure.";
+            }else {
+                return "Calm down, I know what I'm doing!";
+            }
         }
         if(unnecessaryPunctuationFree.equals(unnecessaryPunctuationFree.toUpperCase())){
             return "Whoa, chill out!";
