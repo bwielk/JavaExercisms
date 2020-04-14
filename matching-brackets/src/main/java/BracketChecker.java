2444c7ea25b2955987f1f04133d0a56e04fae8d0
bwielk <bwielk@gmail.com>
2020-04-14 14:59:45 +0200

    matching brackets - extracting nested brackets - further improvements

--- matching-brackets/src/main/java/BracketChecker.java
+++ matching-brackets/src/main/java/BracketChecker.java
@@ -18,13 +18,13 @@ class BracketChecker {
         while(attemptsToFindAnExistingPairOfBrackets<=3){
             for(String acceptedBracket : acceptedBracketPairs){
                 if(bracketsCopy.contains(acceptedBracket)){
-                    reorderedBrackets.add(acceptedBracket);
                     int beginningIndex = bracketsCopy.indexOf(acceptedBracket);
                     char[] brokenBrackets = bracketsCopy.toCharArray();
                     brokenBrackets[beginningIndex] = ' ';
                     brokenBrackets[beginningIndex+1] = ' ';
                     bracketsCopy = String.valueOf(brokenBrackets).replaceAll(" " , "");
                     attemptsToFindAnExistingPairOfBrackets=0;
+                    this.brackets += acceptedBracket;
                 }else{
                     attemptsToFindAnExistingPairOfBrackets++;
                     if(attemptsToFindAnExistingPairOfBrackets == 3){
@@ -36,7 +36,7 @@ class BracketChecker {
         if(brackets.length()%2!=0){
             return false;
         }
-        this.brackets = String.join("", reorderedBrackets);
+
         for(int i=0; i<brackets.length(); i+=2){
             String pair = new StringBuffer().append(brackets.charAt(i)).append(brackets.charAt(i+1)).toString();
                 if(!acceptedBracketPairs.contains(pair)) {
