fe6fed300255a7c0591680db04bdce411f2fbc98
bwielk <bwielk@gmail.com>
2020-05-13 10:08:10 +0200

    grades - structure

---
+++ grade-school/src/main/java/School.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class School {
+
+    public void add(String name, Integer mark){
+
+    }
+
+    public List<String> roster(){
+        List<String> results = new ArrayList<>();
+        return results;
+    }
+
+    public List<String> grade(Integer grade){
+        List<String> results = new ArrayList<>();
+        return results;
+    }
+
+}
