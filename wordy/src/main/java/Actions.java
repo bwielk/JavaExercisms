9543c7bac5499e5b09a66185999c43dae4f1bae9
bwielk <bwielk@gmail.com>
2020-05-13 20:15:26 +0200

    Wordy - expanded structure

---
+++ wordy/src/main/java/Actions.java
@@ -0,0 +1,7 @@
+public class Actions {
+
+    public static final String ADD = "plus";
+    public static final String SUBTRACT = "minus";
+    public static final String MULTIPLY = "multipl";
+    public static final String DIVIDED = "divided";
+}
--- wordy/src/main/java/WordProblemSolver.java
+++ wordy/src/main/java/WordProblemSolver.java
@@ -1,47 +1,64 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 class WordProblemSolver {
 
-    public int solve(String sentence){
+    public int solve(String sentence) {
         String foundNumberAsChars = "";
         List<Integer> foundNumbers = new ArrayList<>();
-        for(int c=0; c<sentence.length(); c++){
-            if(Character.isDigit(sentence.charAt(c))){
-                try{
-                    if(sentence.charAt(c-1)=='-'){
-                        foundNumberAsChars+=sentence.charAt(c-1);
+        List<String> foundActions = new ArrayList<>();
+
+        Arrays.asList(sentence.split(" ")).forEach(x -> {
+            if (x.contains(Actions.ADD) || x.contains(Actions.SUBTRACT)
+                    || x.contains(Actions.MULTIPLY) || x.contains(Actions.DIVIDED)) {
+                foundActions.add(x);
+            }
+        });
+
+        for (int c = 0; c < sentence.length(); c++) {
+            if (Character.isDigit(sentence.charAt(c))) {
+                try {
+                    if (sentence.charAt(c - 1) == '-') {
+                        foundNumberAsChars += sentence.charAt(c - 1);
                     }
-                }catch(ArrayIndexOutOfBoundsException e){
-                    System.out.println(e);
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    System.out.println(e.getMessage());
                 }
 
                 int startingCharForIntSearch = c;
-                while(Character.isDigit(sentence.charAt(startingCharForIntSearch))){
-                    foundNumberAsChars+=sentence.charAt(startingCharForIntSearch);
-                    startingCharForIntSearch +=1;
+                while (Character.isDigit(sentence.charAt(startingCharForIntSearch))) {
+                    foundNumberAsChars += sentence.charAt(startingCharForIntSearch);
+                    startingCharForIntSearch += 1;
                 }
-                c=startingCharForIntSearch;
+                c = startingCharForIntSearch;
                 foundNumbers.add(Integer.parseInt(foundNumberAsChars));
                 foundNumberAsChars = "";
             }
         }
-        if(foundNumbers.size() == 1){
-            return foundNumbers.get(0);
-        }else{
-            if(sentence.contains("plus")){
-                return foundNumbers.stream().reduce(0, Integer::sum);
-            }
-            if(sentence.contains("minus")){
-                return foundNumbers.get(0)-foundNumbers.get(1);
-            }
-            if(sentence.contains("multipl")){
-                return  foundNumbers.get(0)*foundNumbers.get(1);
-            }
-            if(sentence.contains("divide")){
-                return  foundNumbers.get(0)/foundNumbers.get(1);
+
+        int total = 0;
+        if (foundNumbers.size() == 1) {
+            return foundNumbers.get(0);}
+
+        for (int action = 0; action < foundActions.size(); action++) {
+            for (int number = 0; number < foundNumbers.size(); number++) {
+                switch (foundActions.get(action)) {
+                    case Actions.ADD:
+                        foundNumbers.stream().reduce(0, Integer::sum);
+                        break;
+                    case Actions.SUBTRACT:
+                        foundNumbers.get(0) - foundNumbers.get(1);
+                        break;
+                    case Actions.MULTIPLY:
+                        foundNumbers.get(0) * foundNumbers.get(1);
+                        break;
+                    case Actions.DIVIDED:
+                        foundNumbers.get(0) / foundNumbers.get(1);
+                        break;
+                }
             }
         }
-        return 0;
+        return total;
     }
 }
