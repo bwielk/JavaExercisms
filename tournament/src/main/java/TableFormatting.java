dd359e79cb16208952f97a2305fa87239d6804f4
bwielk <bwielk@gmail.com>
2020-05-13 13:35:17 +0200

    applied a fix - too many quotes disrupting the table

--- tournament/src/main/java/TableFormatting.java
+++ tournament/src/main/java/TableFormatting.java
@@ -1,5 +1,5 @@
 public class TableFormatting {
 
     public static final String TABLE_HEADER = "Team                           | MP |  W |  D |  L |  P\n";
-    public static final String TABLE_ROW = "\"%s|  %s |  %s |  %s |  %s |  %s\\n\"";
+    public static final String TABLE_ROW = "%s|  %s |  %s |  %s |  %s |  %s\n";
 }
