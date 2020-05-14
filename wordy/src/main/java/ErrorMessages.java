6536b5f4b8023e0a0a4a7513ab5fa9046267edab
bwielk <bwielk@gmail.com>
2020-05-14 19:20:03 +0200

    Wordy - refactoring and adding more java docs

---
+++ wordy/src/main/java/ErrorMessages.java
@@ -0,0 +1,4 @@
+public class ErrorMessages {
+
+    public static final String QUESTION_NOT_UNDERSTOOD = "I'm sorry, I don't understand the question!";
+}
--- wordy/src/main/java/WordProblemSolver.java
+++ wordy/src/main/java/WordProblemSolver.java
@@ -6,12 +6,17 @@ class WordProblemSolver {
 
     public int solve(String sentence) {
 
+        /**
+         * Instantiate lists storing the data we'll be iterating through
+         */
         String regexForCheckingNumbers = "-?\\d+(.\\d+)?";
-
         List<Integer> foundNumbers = new ArrayList<>();
         List<String> foundActions = new ArrayList<>();
         List<String> splitSentence = Arrays.asList(sentence.replace("?", "").split(" "));
 
+        /**
+         * Identify keywords and digits to manipulate with
+         */
         for(int word=0; word<splitSentence.size(); word++){
             String x = splitSentence.get(word);
             if (x.contains(Actions.ADD) || x.contains(Actions.SUBTRACT)
@@ -20,64 +25,70 @@ class WordProblemSolver {
             }
             if (x.matches(regexForCheckingNumbers)){
                 foundNumbers.add(Integer.parseInt(x));
-                try{
-                    if(splitSentence.get(word+1).matches(regexForCheckingNumbers)){
-                        throw new IllegalArgumentException("I'm sorry, I don't understand the question!");
-                    }
-                }catch(ArrayIndexOutOfBoundsException e){
-                    System.out.print(e.getMessage());
+                if(word+1<splitSentence.size() && splitSentence.get(word+1).matches(regexForCheckingNumbers)){
+                        throw new IllegalArgumentException(ErrorMessages.QUESTION_NOT_UNDERSTOOD);
                 }
-
             }
         }
 
+        /**
+         * After picking up the words and numbers, verify that the input makes sense at all
+         */
+
         if(foundNumbers.size() == 0){
-            throw new IllegalArgumentException("I'm sorry, I don't understand the question!");
+            throw new IllegalArgumentException(ErrorMessages.QUESTION_NOT_UNDERSTOOD);
         }
 
         if (foundNumbers.size() == 1 && String.format(Actions.WHAT_IS, foundNumbers.get(0)).equals(sentence)) {
             return foundNumbers.get(0);}
 
         if (foundActions.size() == 0 && !String.format(Actions.WHAT_IS, foundNumbers.get(0)).equals(sentence)){
-            throw new IllegalArgumentException("I'm sorry, I don't understand the question!");
+            throw new IllegalArgumentException(ErrorMessages.QUESTION_NOT_UNDERSTOOD);
         }
 
         if (foundNumbers.size() != foundActions.size()+1){
-            throw new IllegalArgumentException("I'm sorry, I don't understand the question!");
+            throw new IllegalArgumentException(ErrorMessages.QUESTION_NOT_UNDERSTOOD);
         }
+
+        /**
+         * Begin the checks.
+         *
+         * int numberIndexLimit - defines that we're going to act on two operands at a time. This
+         *    value will be increased by one after an operation has been completed
+         * int currentNumberToManipulate - the first number we're going to manipulate. Later on that
+         *    number becomes a total of the n+1 operation, and eventually will result in the total
+         */
 
         int numberIndexLimit = 2;
         int currentIndexNumber= 1;
         int currentNumberToManipulate = foundNumbers.get(0);
         int total = 0;
 
-        for (int action = 0; action < foundActions.size(); action++) {
+        for (String foundAction : foundActions) {
             for (int number = currentIndexNumber; number < numberIndexLimit; number++) {
-                int result = 0;
-                switch (foundActions.get(action)) {
+                switch (foundAction) {
                     case Actions.ADD:
-                        result = currentNumberToManipulate + foundNumbers.get(number);
+                        total = currentNumberToManipulate + foundNumbers.get(number);
                         break;
                     case Actions.SUBTRACT:
-                        result = currentNumberToManipulate - foundNumbers.get(number);
+                        total = currentNumberToManipulate - foundNumbers.get(number);
                         break;
                     case Actions.MULTIPLY:
-                        result = currentNumberToManipulate * foundNumbers.get(number);
+                        total = currentNumberToManipulate * foundNumbers.get(number);
                         break;
                     case Actions.DIVIDED:
-                        result = currentNumberToManipulate / foundNumbers.get(number);
+                        total = currentNumberToManipulate / foundNumbers.get(number);
                         break;
                 }
-                currentNumberToManipulate=result;
-                if(number+1<=numberIndexLimit){
+                currentNumberToManipulate = total;
+                if (number + 1 <= numberIndexLimit) {
                     currentIndexNumber++;
                 }
             }
-            if(currentIndexNumber<foundNumbers.size()){
+            if (currentIndexNumber < foundNumbers.size()) {
                 numberIndexLimit++;
             }
         }
-        total += currentNumberToManipulate;
         return total;
     }
 }
