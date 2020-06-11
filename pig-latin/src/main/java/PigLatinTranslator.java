d3106d2e3541686b3fbf130a80b43e2addedc8e3
bwielk <bwielk@gmail.com>
2020-06-11 16:10:58 +0200

    Pig latin - works.

--- pig-latin/src/main/java/PigLatinTranslator.java
+++ pig-latin/src/main/java/PigLatinTranslator.java
@@ -1,11 +1,17 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 class PigLatinTranslator{
 
     private List<String> vowels;
     private List<String> consonantClusters;
+
+    public static final String CONSONANTS_CLUSTERS = "[^aeiou\\s]";
+    public static final String CONSONANTS_PROCEEDED_BY_Y = CONSONANTS_CLUSTERS + "+y";
+    public static final String CONSONANTS_PROCEEDED_B_Y_AND_CHARS = CONSONANTS_PROCEEDED_BY_Y +"+\\w+";
 
     public PigLatinTranslator(){
         vowels = Arrays.asList("a", "e", "i", "o", "u", "xr", "yt");
@@ -23,12 +29,25 @@ class PigLatinTranslator{
     }
 
     public String conductWordTranslation(String word){
+        if(word.matches(CONSONANTS_PROCEEDED_B_Y_AND_CHARS) && word.length()>2){
+            //only the consonant cluster at the beginning of the word we're interested in, hence adding ^
+            Pattern p = Pattern.compile(CONSONANTS_PROCEEDED_BY_Y);
+            Matcher m = p.matcher(word);
+            if(m.find()){
+                //m.end - 1 -> reason: to exclude the "y" found by Pattern p
+                return word.substring(m.end()-1) + word.substring(m.start(), m.end()-1) + "ay";
+            }
+        }
         if(vowels.stream().anyMatch(v-> word.substring(0, v.length()).equals(v))){
             return word + "ay";
         }else{
             for(String cluster : consonantClusters) {
-                if(cluster.equals(word.substring(0, cluster.length()))) {
-                    return word.substring(cluster.length()) + cluster + "ay";
+                try{
+                    if(cluster.equals(word.substring(0, cluster.length()))) {
+                        return word.substring(cluster.length()) + cluster + "ay";
+                    }
+                }catch (IndexOutOfBoundsException e){
+                    e.printStackTrace();
                 }
             }
             return word.substring(1) + word.charAt(0) + "ay";
