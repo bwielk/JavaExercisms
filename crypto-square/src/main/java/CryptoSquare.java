319c50ab3ea4a53c2f55e41a8d419f208fbdfeb6
bwielk <bwielk@gmail.com>
2020-06-29 23:55:17 +0200

    crypto square - implemented processing of imperfect squares

--- crypto-square/src/main/java/CryptoSquare.java
+++ crypto-square/src/main/java/CryptoSquare.java
@@ -1,4 +1,7 @@
+import com.sun.deploy.util.StringUtils;
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 class CryptoSquare {
@@ -16,7 +19,16 @@ class CryptoSquare {
         if(textToProcess.length() > 1){
             int aSide = (int) Math.round(Math.sqrt(textToProcess.length()));
             for(int c=0; c<textToProcess.length(); c+=aSide){
-                matrixSplit.add(textToProcess.substring(c, c+aSide));
+                try{
+                    matrixSplit.add(textToProcess.substring(c, c+aSide));
+                }catch(IndexOutOfBoundsException e){
+                    String stringToAppend = textToProcess.substring(c);
+                    if(stringToAppend.length() < aSide){
+                        stringToAppend += String.join("", Collections.nCopies(aSide-stringToAppend.length(), " "));
+                        matrixSplit.add(stringToAppend);
+                    }
+                }
+
             }
             for(int charIndex = 0; charIndex<aSide; charIndex++){
                 String newWord = "";
